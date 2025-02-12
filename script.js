d8d3a8185dd5e15f862ce8bcd617bb749e16437e
pedropatriciio <pedropatricio2002@yahoo.com>
2025-02-12 13:43:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,22 @@ function removerFormatacao() {
 
     const documentoSemFormatacao = documento.replace(/[^\d]+/g, '');
 
-    document.getElementById("resultado").textContent = "Resultado: " + documentoSemFormatacao;
+    document.getElementById("resultado").textContent = "" + documentoSemFormatacao;
+}
+
+function copiar() {
+    const textoFormatado = document.getElementById("resultado");
+    const texto = textoFormatado.innerText;
+
+    const textarea = document.createElement("textarea");
+    textarea.value = texto;
+    document.body.appendChild(textarea);
+
+    textarea.select();
+    textarea.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(textarea.value);
+
+    document.body.removeChild(textarea);
+
+    alert("Texto copiado para a área de transferência!");
 }
